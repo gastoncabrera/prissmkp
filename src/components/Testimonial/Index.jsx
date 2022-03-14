406bb1e00d08d0e2a63a1a11c3cff9dc5c588979
Cabrera Gaston <cabreragastonivan31@gmail.com>
2022-03-14 00:53:10 -0300

    build_5

--- src/components/Testimonial/Index.jsx
+++ src/components/Testimonial/Index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./styles.module.scss";
-import Carousel from "../Carousel";
+import Carousel from "../Carousel/Index.jsx";
 
 export default function Index() {
   return (
